576b2ba0cd8a0e9d729712443cf256ab03c50683
AniketSanghi <sanghi@iitk.ac.in>
2019-09-04 15:33:37 +0530

    final

--- run.sh
+++ run.sh
@@ -1,5 +1,100 @@
 #!/bin/bash
 
+function printTable()
+{
+    local -r delimiter="${1}"
+    local -r data="$(removeEmptyLines "${2}")"
+
+    if [[ "${delimiter}" != '' && "$(isEmptyString "${data}")" = 'false' ]]
+    then
+        local -r numberOfLines="$(wc -l <<< "${data}")"
+
+        if [[ "${numberOfLines}" -gt '0' ]]
+        then
+            local table=''
+            local i=1
+
+            for ((i = 1; i <= "${numberOfLines}"; i = i + 1))
+            do
+                local line=''
+                line="$(sed "${i}q;d" <<< "${data}")"
+
+                local numberOfColumns='0'
+                numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
+
+                # Add Line Delimiter
+
+                if [[ "${i}" -eq '1' ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+
+                # Add Header Or Body
+
+                table="${table}\n"
+
+                local j=1
+
+                for ((j = 1; j <= "${numberOfColumns}"; j = j + 1))
+                do
+                    table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
+                done
+
+                table="${table}#|\n"
+
+                # Add Line Delimiter
+
+                if [[ "${i}" -eq '1' ]] || [[ "${numberOfLines}" -gt '1' && "${i}" -eq "${numberOfLines}" ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+            done
+
+            if [[ "$(isEmptyString "${table}")" = 'false' ]]
+            then
+                echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'
+            fi
+        fi
+    fi
+}
+
+function removeEmptyLines()
+{
+    local -r content="${1}"
+
+    echo -e "${content}" | sed '/^\s*$/d'
+}
+
+function repeatString()
+{
+    local -r string="${1}"
+    local -r numberToRepeat="${2}"
+
+    if [[ "${string}" != '' && "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]
+    then
+        local -r result="$(printf "%${numberToRepeat}s")"
+        echo -e "${result// /${string}}"
+    fi
+}
+
+function isEmptyString()
+{
+    local -r string="${1}"
+
+    if [[ "$(trimString "${string}")" = '' ]]
+    then
+        echo 'true' && return 0
+    fi
+
+    echo 'false' && return 1
+}
+
+function trimString()
+{
+    local -r string="${1}"
+
+    sed 's,^[[:blank:]]*,,' <<< "${string}" | sed 's,[[:blank:]]*$,,'
+}
 
 function check () {
 	if grep -Ei "^[^,]*$1" AddressBook.txt > /dev/null
@@ -12,7 +107,8 @@ function check () {
 
 function insert() {
 	echo "$1, $2, $3" >> AddressBook.txt
-	echo "Successfully Inserted! "
+	echo "
+	Successfully Inserted! "
 	sleep 1s
 }
 
@@ -20,29 +116,77 @@ function remove() {
 	sed "/$1/d" AddressBook.txt > temp.txt
 	cat temp.txt > AddressBook.txt
 	rm temp.txt
-	echo "Successfully Deleted! "
+	echo "
+	Successfully Deleted! "
 	sleep 1s
 }
 
 function edit() {
 	sketch
 	printf "\n\n"
-	echo "$1"
-	printf "\e[95m Enter the credentials to update \e[295m\n"
-	printf "\e[34m Name: "
-	read name
-	printf "\e[34m Team: "
-	read team
-	printf "\e[34m Batting average: "
-	read avg
+	echo "Player Name,Team Name,Batting Avg" > temp.txt
+	echo "$1">> temp.txt
+	printTable ',' "$(cat temp.txt)"
+	printf "\n\n\e[95m Which credential to update \e[295m\n\n"
+	printf "\e[96m	1.	Player Name \e[296m\n"
+	printf "\e[96m	2.	Team Name\e[296m\n"
+	printf "\e[96m	3.	Batting Average \e[296m\n"
+	printf "\e[96m	4.	Main Menu \e[296m\n"
+	read reply
 
-	sed "s/$1/$name, $team, $avg/g" AddressBook.txt > temp.txt
-	cat temp.txt > AddressBook.txt
-	rm temp.txt
+	if [ $reply = '1' ]
+	then
+		echo "Enter the Player's Name"
+		read name
+		awk -F, -v a="${1}" -v b=$name '$0 ~ a {$1=b} 1' OFS="," AddressBook.txt > temp.txt
+		cat temp.txt > AddressBook.txt
+		rm temp.txt
 
-	echo "Successfully Edited!"
+		echo "
+		Successfully Edited!"
 
-	sleep 1s
+		sleep 1s
+
+		edit "`awk -F, -v a="${1}" -v b=$name '$0 ~ a {$1=b} 1' OFS="," <<< $1`"
+		
+	elif [ $reply = '2' ]
+	then
+		echo "Enter the Team's Name"
+		read team
+		awk -F, -v a="${1}" -v b=$team '$0 ~ a {$2=b} 1' OFS="," AddressBook.txt > temp.txt
+		cat temp.txt > AddressBook.txt
+		rm temp.txt
+
+		echo "
+		Successfully Edited!"
+
+		sleep 1s
+
+		edit "`awk -F, -v a="${1}" -v b=$team '$0 ~ a {$2=b} 1' OFS="," <<< $1`"
+
+
+	elif [ $reply = '3' ]
+	then
+		echo "Enter the Batting average"
+		read avg
+		awk -F, -v a="${1}" -v b=$avg '$0 ~ a {$3=b} 1' OFS="," AddressBook.txt > temp.txt
+		cat temp.txt > AddressBook.txt
+		rm temp.txt	
+
+		echo "
+		Successfully Edited!"
+
+		sleep 1s
+
+		edit "`awk -F, -v a="${1}" -v b=$avg '$0 ~ a {$3=b} 1' OFS="," <<< $1`"
+
+		
+	else
+		main
+	fi
+
+
+
 }
 
 function searchByName() {
@@ -52,14 +196,16 @@ function searchByName() {
 	then
 		echo""
 	else 
-		echo "No Data Found"
+		echo "No Data Found!!!"
 		sleep 1s
 		main
 	fi
-	grep -Ei "^[^,]*$1" AddressBook.txt | column -s "," -t | awk '{print NR":	"$0}' 
+	echo "S.No.,Player Name,Team Name,Batting Avg" > temp.txt
+	grep -Ei "^[^,]*$1" AddressBook.txt | awk '{print NR","$0}' >> temp.txt
+	printTable ',' "$(cat temp.txt)"
 	printf "\e[31m	\n\nWhat you wanna do?\e[231m\n\n"
-	printf "\e[96m	1.	Remove\e[296m\n"
-	printf "\e[96m	2.	Update\e[296m\n"
+	printf "\e[96m	1.	Remove an entry\e[296m\n"
+	printf "\e[96m	2.	Update an entry\e[296m\n"
 	printf "\e[96m	3.	Add New Entry\e[296m\n"
 	printf "\e[96m	4.	Go Back! \e[296m\n"
 	printf "\e[93m  \nChoose your option number and input it ( eg : 1) \e[234m\n"
@@ -108,14 +254,16 @@ function searchByTeam() {
 	then
 		echo""
 	else 
-		echo "No Data Found"
+		echo "No Data Found!!!"
 		sleep 1s
 		main
 	fi
-	grep -Ei "^[^,]*,[^,]*$1" AddressBook.txt | column -s "," -t | awk '{print NR":	"$0}'
+	echo "S.No.,Player Name,Team Name,Batting Avg" > temp.txt
+	grep -Ei "^[^,]*,[^,]*$1" AddressBook.txt | awk '{print NR","$0}' >> temp.txt
+	printTable ',' "$(cat temp.txt)"
 	printf "\e[31m	\n\nWhat you wanna do?\e[231m\n\n"
-	printf "\e[96m	1.	Remove\e[296m\n"
-	printf "\e[96m	2.	Edit\e[296m\n"
+	printf "\e[96m	1.	Remove an entry\e[296m\n"
+	printf "\e[96m	2.	Edit an entry\e[296m\n"
 	printf "\e[96m	3.	Add New Entry\e[296m\n"
 	printf "\e[96m	4.	Go Back! \e[296m\n"
 	printf "\e[93m  \nChoose your option number and input it ( eg : 1) \e[234m\n"
@@ -140,10 +288,6 @@ function searchByTeam() {
 
 	elif [ $reply = '3' ]
 	then
-		if [ $# -gt '0' ]
-		then
-			insert $1 $2 $3
-		else 
 			printf "\e[95m Enter the credentials \e[295m\n"
 			echo "Name: "
 			read name
@@ -152,7 +296,7 @@ function searchByTeam() {
 			echo "Batting average: "
 			read avg
 			insert $name $team $avg
-		fi
+		
 	else
 		main
 	fi
@@ -174,8 +318,8 @@ function main() {
 	do
 		sketch
 		printf "\e[31m	Operations Available:\e[231m\n\n"
-		printf "\e[96m	1.	Insert\e[296m\n"
-		printf "\e[96m	2.	Search\e[296m\n"
+		printf "\e[96m	1.	Insert an entry\e[296m\n"
+		printf "\e[96m	2.	Search for an entry\e[296m\n"
 		printf "\e[96m	3.	Exit\e[296m\n"
 		printf "\e[93m  \nChoose your option number and input it ( eg : 1) \e[234m\n"
 		printf "\e[31m  The match is not case sensitive match. \e[231m\n\n"
